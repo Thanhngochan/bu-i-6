2e13a612af401e1ee214db58dd8e29a6798f635d
Thanhngochan <164725419+Thanhngochan@users.noreply.github.com>
2024-03-31 15:34:40 +0800

    Add files via upload

---
+++ b6.py
@@ -0,0 +1,30 @@
+
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     if n <= 3:
+#         return True
+#     if n % 2 == 0 or n % 3 == 0:
+#         return False
+#     i = 5
+#     while i * i <= n:
+#         if n % i == 0 or n % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
+
+# # Ví dụ sử dụng
+# number = int(input("Nhập một số nguyên dương: "))
+# if is_prime(number):
+#     print(number, "là số nguyên tố")
+# else:
+#     print(number, "không là số nguyên tố")
+
+# vừng ơi mở ra 
+for i in range:
+ i=str(input("vừng ơi mở ra:"))
+ if i=="python":
+     print("xin chào")
+ else:
+     print("mật khẩu sai, vui lòng nhập lại")
+
